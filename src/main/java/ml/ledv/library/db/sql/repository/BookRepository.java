4d9374a89ee8cd64e3b7a18a89c7d67021dd907e
dzianis.leshaniuk <dzianis.leshaniuk@instinctools.ru>
2017-12-26 17:29:01 +0300

    Added BookService implementation.

--- src/main/java/ml/ledv/library/db/sql/repository/BookRepository.java
+++ src/main/java/ml/ledv/library/db/sql/repository/BookRepository.java
@@ -3,5 +3,9 @@ package ml.ledv.library.db.sql.repository;
 import ml.ledv.library.db.sql.entity.impl.BookEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface BookRepository extends PagingAndSortingRepository<BookEntity, String> {
+
+    List<BookEntity> getBookEntitiesByUserEntityIsNull();
 }
--- src/main/java/ml/ledv/library/db/sql/service/impl/BookServiceImpl.java
+++ src/main/java/ml/ledv/library/db/sql/service/impl/BookServiceImpl.java
@@ -1,10 +1,11 @@
 package ml.ledv.library.db.sql.service.impl;
 
-import ml.ledv.library.db.nosql.entity.Book;
-import ml.ledv.library.db.nosql.service.BookService;
+
 import ml.ledv.library.db.sql.entity.impl.BookEntity;
+import ml.ledv.library.db.sql.entity.impl.UserEntity;
 import ml.ledv.library.db.sql.repository.BookRepository;
 import ml.ledv.library.db.sql.repository.UserRepository;
+import ml.ledv.library.db.sql.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -22,41 +23,50 @@ public class BookServiceImpl implements BookService {
         this.userRepository = userRepository;
     }
 
+
     @Override
-    public void createBook(final String login) {
-
+    public void createBook(final String name) {
         final BookEntity bookEntity = new BookEntity();
-        bookEntity.setName(login);
+        bookEntity.setName(name);
 
+        bookRepository.save(bookEntity);
     }
 
     @Override
-    public void deleteBook(final Book book) {
+    public void deleteBook(final BookEntity book) {
         bookRepository.delete(book);
     }
 
     @Override
-    public List<Book> getAll() {
-        return null;
+    public List<BookEntity> getAll() {
+        return (List<BookEntity>) bookRepository.findAll();
     }
 
     @Override
-    public List<Book> getAllFree() {
-        return null;
+    public List<BookEntity> getAllFree() {
+        return bookRepository.getBookEntitiesByUserEntityIsNull();
     }
 
     @Override
-    public Optional<Book> getBookById(final String id) {
-        return Optional.empty();
+    public Optional<BookEntity> getBookById(final String id) {
+        return bookRepository.findById(id);
     }
 
     @Override
-    public void updateBook(final Book book) {
-
+    public void updateBook(final BookEntity book) {
+        bookRepository.save(book);
     }
 
     @Override
-    public void removeUser(final Book book) {
+    public void removeUser(final BookEntity book) {
 
+        final UserEntity userEntity = book.getUserEntity();
+
+        book.setUserEntity(null);
+
+        userEntity.getBooks().remove(book);
+
+        userRepository.save(userEntity);
+        bookRepository.save(book);
     }
 }
